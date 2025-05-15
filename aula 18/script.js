311662d4f40b5bc0c942ac4e976e3d10c27b179b
JoaoArthur615 <joaops5eu@gmail.com>
2025-05-14 22:36:19 -0300

    Add files via upload

---
+++ aula 18/script.js
@@ -0,0 +1,18 @@
+document.write ("<h2> Olá Mundo! Esse é meu primeiro progama Jar jar binx </h2> <br>");
+document.write ("<p> i wanna be the guy </p>");
+
+   /* window.alert ("Negativo amiguinho"); */
+document.getElementById('ola').innerHTML= 'ola como você está '
+
+document.getElementById ("resposta").innerHTML = window.confirm ("do you like hurt people")
+
+document.getElementById("resposta").innerHTML = "O nome digitado foi : " + window.prompt("Qual é seu nome ?")
+
+
+    function abrepagina ()
+    {
+     window.open("nova pagina.html");
+
+
+    }   
+
---
+++ aula 18/script2.js
@@ -0,0 +1,11 @@
+function voltar ()
+{
+ window.open("cc1n.html");
+
+}
+
+
+function exibir ()
+{
+ window.alert ("Nome digitado foi:" + document.getElementById ("nome").value );
+}
